8974b12fc0c8626c07073cc3d6736b032c9b2c11
yotam-bp <yotyyybp@gmail.com>
2021-08-05 18:12:14 +0300

    setting app.js, api

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -11,3 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
