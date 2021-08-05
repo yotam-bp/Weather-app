c14e2aa28d99c9d768c032686379b0a7c61d0e29
yotam-bp <yotyyybp@gmail.com>
2021-08-05 19:33:46 +0300

    custom hook for search, weather list cmp

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-// import CitySelector from './components/CitySelector';
-// import './App.css';
+import CitySelector from './components/CitySelector';
+import './App.css';
 import {Container} from 'react-bootstrap';
+import UseFetch from './hooks/UseFetch'
+import {API_KEY, API_BASE_URL} from './api/config';
 
 const App = () => {
+  const {data, error, isLoading, setUrl} = UseFetch();
+
   return (
     <Container className="App">
-      {/* <CitySelector /> */}
+        <CitySelector onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}`)} />
     </Container>
   );
 };
