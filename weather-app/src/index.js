097078019d794cf5eb5dc4ca99c6dfa8478832d4
yotam-bp <yotyyybp@gmail.com>
2021-08-06 17:23:00 +0300

    add error description

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -9,3 +9,9 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+// Error: Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
+// 1. You might have mismatching versions of React and the renderer (such as React DOM)
+// 2. You might be breaking the Rules of Hooks
+// 3. You might have more than one copy of React in the same app
+// See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.
