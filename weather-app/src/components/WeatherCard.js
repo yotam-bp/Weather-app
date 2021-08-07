0d21394f6bc3ca1e1b402b9439e88dc2f03d43ea
yotam-bp <yotyyybp@gmail.com>
2021-08-07 13:59:15 +0300

    change theme ad convert

--- weather-app/src/components/WeatherCard.js
+++ weather-app/src/components/WeatherCard.js
@@ -1,11 +1,16 @@
 
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
-const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
+const WeatherCard = ({ dt, temp_min, temp_max, main, icon }) => {
   const date = new Date(dt);
+
+   convertToCels = (temp_min) => {
+    console.log(temp_min);
+    return (fahrenheit - 32) * 5 / 9
+  }
   return (
-    <Card style={{width: '18rem'}}>
+    <Card style={{ width: '18rem' }}>
       <Card.Img
         variant="top"
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
@@ -15,8 +20,8 @@ const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
         <p>
           {date.toLocaleDateString()} - {date.toLocaleTimeString()}
         </p>
-        <p>Min: {temp_min}</p>
-        <p>Max: {temp_max}</p>
+        <p>Min:1</p>
+        <p>Max: 2</p>
       </Card.Body>
     </Card>
   );
