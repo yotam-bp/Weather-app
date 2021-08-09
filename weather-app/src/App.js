10e36a5a2ab88c420d9674acca71e75588356711
yotam-bp <yotyyybp@gmail.com>
2021-08-09 23:46:26 +0300

    autocomplete bug

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import CitySelector from './components/CitySelector';
-import AutoComplete from './components/AutoComplete';
+// import AutoComplete from './components/AutoComplete';
 import { Container } from 'react-bootstrap';
 import UseFetch from './hooks/UseFetch';
 import { API_KEY, API_BASE_URL } from './apis/config'
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Container className="App">
       <CitySelector onSearch={(city) => setUrl(`${API_BASE_URL}/data/2.5/forecast?q=${city}&cnt=5&appid=${API_KEY}`)} />
-      <AutoComplete />
+      {/* <AutoComplete /> */}
       {/* conditionally render  */}
       {getContent()}
     </Container>
